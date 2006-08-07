28cff7ddf76140ac96679ac812b1fc06e7ba6da1
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2006-08-07 14:44:50 +0000

    Built using Quartus II V6.0, does not work, Rx fifo full after a few seconds.

--- trunk/VK6APH/Ozy_Janus/load.bat
+++ trunk/VK6APH/Ozy_Janus/load.bat
@@ -1,5 +1,5 @@
 rem Loading monitor.......
-load_firmware 0xfffe 0x0007 OZYV1FW08022006-1.hex
+load_firmware 0xfffe 0x0007 OZYV1FW08042006-1.hex
 pause
 upload_FPGA 0xfffe 0x0007 ozy_janus.rbf
 pause
