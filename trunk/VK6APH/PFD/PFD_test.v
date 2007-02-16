18cbb99a519e44a20a0197ae47e83bf42f09bf76
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2007-02-16 10:50:33 +0000

    Test code for 125MHz PLL

--- trunk/VK6APH/PFD/PFD_test.v
+++ trunk/VK6APH/PFD/PFD_test.v
@@ -1,21 +1,19 @@
 // test code for Phase Frequency Detector
-// Divides both 10MHz reference signal and 12.288MHz VCXO 
-// to give 8kHz signals.  These are fed to a Phase Frequency Detector (PFD)
+// Divides both 10MHz reference signal and 125MHz VCXO 
+// to give 2.5MHz signals.  These are fed to a Phase Frequency Detector (PFD)
 // the output of which is filtered and fed to the VCXO. 
 // The PFD has a lock detector that is active low. 
 // The circuit checks that the 10MHz reference is present, if not 
-// it connects the 1kHz from the VCXO to the PFD output so that 
+// it connects the 2.5MHz from the VCXO to the PFD output so that 
 // the VCXO LPF output is constant at 3.3v/2
 
-// V1.1 11th Nov 2006 changed ref to 8kHz since this is the HCF of 10MHz and 12.288MHz that
-// allows an integer divider for both signals
-// 27th November - changed to use Megafunctions for the dividers and moved to Janus CPLD
 
 
-module PFD_test(ref_in, osc_in, tune, LED1, LED2, C5);
+
+module PFD_test(ref_in, osc_in, tune, LED1, LED2, C5, ref_2_5M, osc_2_5M);
 
 input ref_in;		// 10MHz reference 
-input osc_in;		// 12.288MHz from VCXO
+input osc_in;		// 125MHz from VCXO
 output tune;		// signal to loop LPF
 output LED1;
 output LED2;
@@ -23,8 +21,12 @@ output C5;			// 12MHz signal over Atlas bus
 wire tune;
 wire pfd_out;
 wire lock;			// is low when loop is locked
-reg ref_8k;
-reg osc_8k;
+output ref_2_5M;
+output osc_2_5M;
+
+
+reg ref_2_5M;
+reg osc_2_5M;
 reg ref_OK;			// is low when reference signal is present
 wire cout1;
 wire cout2;
@@ -33,44 +35,69 @@ wire C5;
 assign C5 = osc_in; 
 
 
-// divide 10MHz reference clock by 625 to give  16kHz
-counter1 counter1(.clock(ref_in), .cout(cout1));
+// divide 10MHz reference clock by 4 to give 2.5MHz
 
-// divide by 2 to give a square wave
-always @ (posedge cout1)
-	ref_8k <= ~ref_8k;
+reg [2:0]ref_count;
 
+always @ (posedge ref_in)
+begin
+	if (ref_count == 1)
+	begin
+		ref_2_5M <= ~ref_2_5M;
+		ref_count <= 0;
+	end
+	else ref_count <= ref_count + 1;
+end
 
-// divide 12.288MHz ADC  clock by 768 to give 16kHz
-counter2 counter2(.clock(osc_in), .cout(cout2));
+// divide 125MHz  clock by 50 to give 2.5MHz
 
-// divide by 2 to give a square wave
-always @ (posedge cout2)
-	osc_8k <= ~osc_8k;
+reg [5:0]osc_count;
 
+always @ (posedge osc_in)
+begin
+	if (osc_count == 24)
+	begin
+		osc_2_5M <= ~osc_2_5M;
+		osc_count <= 0;
+	end 
+	else osc_count <= osc_count + 1;
+end
 
 // apply to PFD
 
-pfd Janus_pfd(.ref_in(ref_8k),.osc_in(osc_8k),.pfd_out(pfd_out),.lock(lock));
+//pfd Janus_pfd(.ref_in(ref_2_5M),.osc_in(osc_2_5M),.pfd_out(pfd_out),.lock(lock));
 
 //
 // check if the 10MHz reference signal is present.
-// if so use PFD output else use 4kHz so as to set the 
+// if so use PFD output else use 2.5MHz so as to set the 
 // VCXO control voltage to 3.3v/2 
 //
 
-always @ (posedge ref_in or posedge osc_8k)
-begin 
-if (ref_in)ref_OK <= 1'b0;   // ref_OK goes low if ref present so we can drive an LED
-else ref_OK <= 1'b1;		 // goes high if no reference 
-end 
+//always @ (posedge ref_in or posedge osc_5M)
+//begin 
+//if (ref_in)ref_OK <= 1'b0;   // ref_OK goes low if ref present so we can drive an LED
+//else ref_OK <= 1'b1;		 // goes high if no reference 
+//end 
 
 // select the signal to send to the loop LPF depending if the 10MHz reference is present
-assign  tune = ref_OK ?  osc_8k : pfd_out; 
+//assign  tune = ref_OK ?  osc_5M : pfd_out; 
 
 //assign  tune = pfd_out; 
 
-//assign tune = ~ref_8k ^ osc_8k;
+wire pre_tune = ref_2_5M ^ osc_2_5M;
+//wire pre_tune = pfd_out;
+
+reg pre_tune_d;
+reg pre_tune_dd;
+
+always @(posedge osc_in)
+  begin
+    pre_tune_d <=  pre_tune;
+    pre_tune_dd <= pre_tune_d;
+  end
+
+assign tune = pre_tune_dd;
+
 
 // LEDs for testing
 
