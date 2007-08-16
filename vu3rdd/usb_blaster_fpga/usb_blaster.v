d416c1ad67bcb0915bdcac3b69d8781cfb0ac857
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2007-08-16 05:09:18 +0000

    Changed pin directions

--- vu3rdd/usb_blaster_fpga/usb_blaster.v
+++ vu3rdd/usb_blaster_fpga/usb_blaster.v
@@ -9,29 +9,29 @@
 // B1 - TDO
 // B2 - TCK
 // B3 - TMS
-// A23 - Atlas TMS
-// A24 - Atlas TCK
-// A27 - Atlas TDO
-// A29 - Atlas TDI
+// A23 - Atlas TMS - output
+// A24 - Atlas TCK - output
+// A27 - Atlas TDO - input
+// A29 - Atlas TDI - output
 
 module usb_blaster_emulation(
         B0, B1, B2, B3,
 		A23, A24, A27, A29
 		);
 		
-input B0;
-input B1;
-input B2;
-input B3;
+input  B0;
+output B1;
+input  B2;
+input  B3;
 
 output A23;
 output A24;
-output A27;
+input  A27;
 output A29;
 
 assign A23 = B3;
 assign A24 = B2;
-assign A27 = B1;
+assign B1  = A27;
 assign A29 = B0;
 
 
