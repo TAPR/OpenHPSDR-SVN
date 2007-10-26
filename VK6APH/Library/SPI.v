ab8af3a8c20f730e7e33ee298b058d0a7990070d
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2007-10-26 01:07:31 +0000

    Alex SPI and filter select files added

--- VK6APH/Library/SPI.v
+++ VK6APH/Library/SPI.v
@@ -1,90 +1,111 @@
+// V1.0 25th October 2007
+//
+// Copyright 2006,2007 Phil Harman VK6APH
+//
+//  HPSDR - High Performance Software Defined Radio
+//
+//  Alex SPI interface.
+//
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 //////////////////////////////////////////////////////////////
 //
-// 		Set up TLV320 using SPI 
+//		Alex SPI interface
 //
-/////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
-/* Data to send to TLV320 is 
+/*
+	data to send to Alex Tx filters is in the following format:
 
- 	1E 00 - Reset chip
- 	12 01 - set digital interface active
- 	08 15 - D/A on, mic input, mic 20dB boost
-   (08 14 - ditto but no mic boost)
- 	0C 00 - All chip power on
- 	0E 02 - Slave, 16 bit, I2S
- 	10 00 - 48k, Normal mode
- 	0A 00 - turn D/A mute off
+	Bit 15 - NC			U2 - D0
+	Bit 14 - NC			U2 - D1
+	Bit 13 - NC			U2 - D2
+	Bit 12 - Yellow Led	U2 - D3
+	Bit 11 - 30/20m		U2 - D4
+	Bit 10 - 60/40m		U2 - D5
+	Bit  9 - 80m		U2 - D6
+	Bit  8 - 160m		U2 - D7
+
+	Bit  7 - Ant #1		U4 - D0
+	Bit  6 - Ant #2		U4 - D1
+	Bit  5 - Ant #3		U4 - D2
+	Bit  4 - T/R relay	U4 - D3
+	Bit  3 - Red Led	U4 - D4
+	Bit  2 - 6m			U4 - D5
+	Bit  1 - 12/10m		U4 - D6
+	Bit  0 - 17/15m		U4 - D7
+
+	Relay selection data is contained in [6:0]LPF
+
+	SPI data is sent to Alex whenever any of the above data changes
 
 */
 
-reg index;
-reg [15:0]tdata;
-reg [2:0]load;
-reg [3:0]TLV;
-reg [15:0] TLV_data;
-reg [3:0] bit_cnt;
+module SPI(Alex_data, SPI_data, SPI_clock,  Tx_load_strobe, spi_clock);
 
-// Set up TLV320 data to send 
+input wire[15:0]Alex_data;
+output reg SPI_data;
+output reg SPI_clock;
+output reg Tx_load_strobe;
+input wire spi_clock;
 
-always @ (posedge index)		
+reg [2:0]spi_state;
+reg [3:0]data_count;
+reg [15:0]previous_Alex_data;	// used to detect change in data 
+
+
+always @ (posedge spi_clock)
 begin
-load <= load + 3'b1;			// select next data word to send
-case (load)
-3'd0: tdata <= 16'h1E00;		// data to load into TLV320
-3'd1: tdata <= 16'h1201;
-3'd2: tdata <= 16'h0815;
-3'd3: tdata <= 16'h0C00;
-3'd4: tdata <= 16'h0E02;
-3'd5: tdata <= 16'h1000;
-3'd6: tdata <= 16'h0A00;
-default: load <= 0;
+case (spi_state)
+0:	begin
+		if (Alex_data != previous_Alex_data)begin
+			data_count <= 15;	// set starting bit count to 15
+			spi_state <= 1;
+		end
+		else spi_state <= 0; 	// wait for Alex data to change
+	end		
+1:	begin
+	SPI_data <= Alex_data[data_count];	// set up data to send
+	spi_state <= 2;
+	end
+2:	begin
+	SPI_clock <= 1'b1;	// set clock high
+	spi_state <= 3;
+	end
+3:	begin
+	SPI_clock <= 1'b0;	// set clock low
+	spi_state <= 4;
+	end
+4:	begin
+		if (data_count == 0)begin		// transfer complete
+			Tx_load_strobe <= 1'b1; 	// strobe Tx data
+			spi_state <= 5;
+		end
+		else begin
+	     	data_count <= data_count - 1'b1;
+			spi_state  <= 1;  			// go round again
+		end
+	end
+5:	begin
+	Tx_load_strobe <= 1'b0;				// reset Tx strobe
+	previous_Alex_data <= Alex_data; 	// save current data 
+	spi_state <= 0;						// reset for next run
+	end
 endcase
 end
 
-// State machine to send data to TLV320 via SPI interface
-
-assign CMODE = 1'b1;		// Set to 1 for SPI mode
-
-always @ (posedge CMCLK)	// use 12.5MHz clock for SPI
-begin
-case (TLV)
-4'd0: begin
-         nCS <= 1'b1;                   // set TLV320 CS high
-         bit_cnt <= 4'd15;             	// set starting bit count to 15
-         index <= ~index;               // load next data to send
-         TLV <= TLV + 4'b1;
-      end
- 4'd1: begin
-         nCS <= 1'b0;                   // start data transfer with nCS low
-         TLV_data <= tdata;
-         MOSI <= TLV_data[bit_cnt];    	// set data up
-         TLV <= TLV + 4'b1;
-       end
- 4'd2: begin
-         SSCK <= 1'b1;                  // clock data into TLV320
-         TLV <= TLV + 4'b1;
-       end
- 4'd3: begin
-         SSCK <= 1'b0;                  // reset clock
-         TLV <= TLV + 4'b1;
-       end
- 4'd4: begin
-          if(bit_cnt == 0) begin   		// word transfer is complete, check for any more
-            index <= ~index;
-            TLV <= 4'd5;
-          end
-          else begin
-            bit_cnt <= bit_cnt - 1'b1;
-            TLV <= 4'b1;                   // go round again
-          end
-       end                                 // end transfer
- 4'd5: begin
-         if (load == 7)begin               // stop when all data sent
-            TLV <= 4'd5;                   // hang out here forever
-            nCS <= 1'b1;                   // set CS high
-         end
-         else TLV <= 0;                    // else get next data
-       end
- default: TLV <= 0;
- endcase
- end
+endmodule
