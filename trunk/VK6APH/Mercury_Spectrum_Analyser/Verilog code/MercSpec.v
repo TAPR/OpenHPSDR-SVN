2f9428f8a9d72d5a59b0b9ecc0312d6946a132fd
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2007-12-06 13:08:29 +0000

    Commented out code using USB (libUSB.dll) so code will run in demo mode with no USB connection.

--- trunk/VK6APH/Mercury_Spectrum_Analyser/Verilog code/MercSpec.v
+++ trunk/VK6APH/Mercury_Spectrum_Analyser/Verilog code/MercSpec.v
@@ -32,8 +32,7 @@ module MercSpec(ADC,
 input [15:0]ADC;				// samples from LT2208
 input FPGA_CLK6IN;				// 125MHz clock from Penelope via LVDS
 input IFCLK;                   	// FX2 IFCLOCK - 48MHz
-//inout  [15:0] FX2_FD;           // bidirectional FIFO data to/from the FX2
-output  [15:0] FX2_FD;           // *** use so simulation works
+output  [15:0] FX2_FD;          // data out to FX2
 input FLAGA;
 input FLAGC;
 output SLWR;
@@ -51,7 +50,6 @@ output DEBUG_LED3;
 wire PKEND;
 reg [1:0] state_FX;				// state for FX2
 reg SLOE;						// FX2 data bus enable - active low
-reg SLEN; 						// Put data on FX2 bus
 reg SLRD;						// FX2 read - active low
 reg SLWR;						// FX2 write - active low 
 reg [1:0] FIFO_ADR;				// FX2 register address 
@@ -64,39 +62,35 @@ assign clock = FPGA_CLK6IN;
 
 // use Megafunction to create 4096 word dual port RAM
 
-reg [11:0]address = 4095; // current read address
-reg [11:0]wraddress = 4095; // current write address
+reg [11:0]address = 4095; 		// current read address
+reg [11:0]wraddress = 4095; 	// current write address
 reg write_enable = 0;
 wire [15:0]q;				
 
 RAM_4096_dualport RAM(.data(temp_ADC),.rdaddress(address),.rdclock(IFCLK),.wraddress(wraddress),.wrclock(clock),.wren(write_enable),.q(q));
 
-// RAM is loaded with a 12.5MHz sine wave using sine_table.mif at start up
+// RAM is loaded with 10 cycles of a sine wave using sine_table.mif at start up
 // send this data to the PC via FX2 
 
 
 // read RAM data and send to FX2
 
 wire EP6_ready = FLAGC; 		// high when we can write to EP6
-reg run ;
 
-always @ (negedge IFCLK) // posedge looks cleaner on the simulation
+always @ (posedge IFCLK) 
 begin
 SLOE <= 1'b1;
 SLRD <= 1'b1;
 FIFO_ADR <= 2'b10;				// select EP6
 
-//if (!run)   // run in loop for now
-//begin
+
 case (state_FX)
 	// check Tx FIFO is ready then set Write strobe 
 	0: begin
-		if (EP6_ready) begin  					// if EP6 is ready, write to it and exit this state
-			address <= address + 1'b1;
-			SLEN <= 1'b1;
+		if (EP6_ready) begin  // if EP6 is ready, write to it and exit this state
 	    	state_FX <= 1;
 		end
-		else begin                  			// otherwise, hang out here until fifo is ready
+		else begin            // otherwise, hang out here until fifo is ready
 		SLWR <= 1;
 		state_FX <= 0;  
 		end
@@ -109,26 +103,32 @@ case (state_FX)
 	//  reset SLWR and tristate SLEN
 	2: begin
 		SLWR <= 1;
-		SLEN <= 1'b0;
-//			if (address == 4095)
-//				run <= 1'b1; 
-		state_FX <= 0;
+		//if (address == 4095) // RAM read complete, go load it from ADC again
+		//	state_FX <= 3;
+		//else begin 
+			address <= address + 1'b1;  // increment address here so q can stabilise
+			state_FX <= 0;
+		//end 
 		end
+//	3:  begin
+//		if(wraddress == 4095)
+//			state_FX <= 0;	  // RAM write complete, go sent to PC
+//		else begin
+//			wraddress <= wraddress + 1'b1;
+//			write_enable <= 1'b1; 		// write current data to RAM 
+// 		end 	
 		default: state_FX <= state_FX + 1'b1;
 		endcase
 	end
-//end
-
-// Swap endian so data is correct at PC end
-
-assign FX2_FD[15:8] = q[7:0];
-assign FX2_FD[7:0]  = q[15:8];	
-
-//assign FX2_FD = q; // don't swap to simlify simulation!
- 
 
 
-assign DEBUG_LED0 = 0;
+// DONT *****Swap endian so data is correct at PC end
+
+assign FX2_FD = q; // don't swap to simplify simulation!
+//assign FX2_FD = counter; 
+
+
+assign DEBUG_LED0 = ~EP6_ready;
 assign DEBUG_LED1 = 0;
 assign DEBUG_LED2 = 0;
 assign DEBUG_LED3 = 0;
@@ -161,7 +161,7 @@ write_enable <= 0; 					// disable write to ram
 	begin
 		wraddress <= wraddress + 1'b1;
 		write_enable <= 1'b1; 		// write current data to RAM 
-		if(address == 4095)
+		if(wraddress == 4095)
 		run <= 0;
 	end
 end
