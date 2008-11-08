5cb4025663e8e7225d9de30f022c121e280db026
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2008-11-08 00:44:38 +0000

    Added support for LTC1451 DAC

--- trunk/Phoenix/CPLD/Test1.v
+++ trunk/Phoenix/CPLD/Test1.v
@@ -7,12 +7,19 @@
 // Change log:
 //
 //	6 Nov 2008 - Started!
+//	7 Nov 2008 - Added SPI interface to LTC1451 DAC
+//			   - Added AD9912 phase word decoder 
+
 
 module Test1(
 			  output LED1,			// Yellow LED
 			  output LED2,			// Green LED
+			  input  DDS_CLK,		// 125MHz clock 
 			  input  CLK_48MHZ,
+			  input  CBCLK, 		// 3.072MHz clock from Atlas C8
+			  input  CLRCLK,		// 48kHz clock from Atlas C9
 			  input  PTT,			// PTT from Atlas C15, active high
+			  input  phase_data,	// AD9912 phase data from Atlas C21
 			  output reg I_CLKRX,	// QSD I clock
 			  output reg Q_CLKRX,	// QSD Q clock
 			  output I_CLKTX,		// QSE I clock
@@ -20,14 +27,18 @@ module Test1(
 			  output RXOE1,			// QSD enable 
 			  output RXOE2,
 			  output TXOE1,			// QSE enable 
-			  output TXOE2
+			  output TXOE2,
+			  output reg DIN,		// LTC1451 SPI interface 
+			  output reg DCLK,
+			  output reg DLD
 			  //output IO_UPD,		// AD9912 control signals 
 			  //output CSB,
 			  //output SDIO,
 			  //output SCLK			  
 			  );
 			  
-assign LED1 = 1'b0;  // turn Yellow Led on
+//assign LED1 = 1'b0;  // turn Yellow Led on
+assign LED1 = AD9912_phase[0]; // temp to get phase word code to compile 
 assign LED2 = PTT;   // turn Green  Led on when PTT active 
 
 
@@ -74,7 +85,111 @@ assign Q_CLKTX = Q_CLKRX;
 //
 /////////////////////////////////////////////////////////////
 
-// to do 
+wire[11:0]DAC_data;
+wire spi_clock;
+
+reg [2:0]spi_state;
+reg [3:0]data_count;
+reg [11:0]previous_DAC_data;		// used in future to detect change in data 
+
+assign DAC_data = 12'b011111111111; // set DAC to mid scale for now
+assign spi_clock = CBCLK;
+
+always @ (posedge spi_clock)
+begin
+case (spi_state)
+0:	begin
+		if (DAC_data != previous_DAC_data)begin
+			data_count <= 11;		// set starting bit count to 11
+			spi_state <= 1;
+		end
+		else spi_state <= 0; 		// wait for Alex data to change
+	end		
+1:	begin
+	DIN <= DAC_data[data_count];	// set up data to send
+	spi_state <= 2;
+	end
+2:	begin
+	DCLK <= 1'b1;					// set clock high
+	spi_state <= 3;
+	end
+3:	begin
+	DCLK <= 1'b0;					// set clock low
+	spi_state <= 4;
+	end
+4:	begin
+		if (data_count == 0)begin		// transfer complete
+			DLD <= 1'b1; 				// strobe data
+			spi_state <= 5;
+		end
+		else begin
+	     	data_count <= data_count - 1'b1;
+			spi_state  <= 1;  			// go round again
+		end
+	end
+5:	begin
+	DLD <= 1'b0;						// reset data strobe
+	previous_DAC_data <= DAC_data; 		// save current data 
+	spi_state <= 0;						// reset for next run
+	end
+endcase
+end
+
+//////////////////////////////////////////////////////////////
+//
+// 		Phase Word  receiver
+//
+/////////////////////////////////////////////////////////////
+
+/*
+
+	The Phase Word  encoder in Ozy broadcasts data over the Atlas bus (C21) 
+	the phase word used by the AD9912.  The data is in 
+	I2S format with the clock being CBLCK and the start of each frame
+	being indicated using the negative edge of CLRCLK.
+	
+	The data format is as follows:
+	
+	<[31:0]phase_word>
+	
+*/
+
+reg [5:0] bits;     // how many bits clocked 
+reg [1:0]CC_state;
+reg [31:0] phase_word;	
+
+always @(posedge CBCLK)  // use CBCLK  from Atlas C8 
+begin
+case(CC_state)
+0:	begin
+	if (CLRCLK == 0)CC_state <= 0;			// loop until CLRLCK is high   
+	else CC_state <= 1;
+	end
+1:	begin
+		if (CLRCLK)	CC_state <= 1;			// loop until CLRCLK is low  
+		else begin
+		bits <= 6'd31;						
+		CC_state <= 2;
+		end
+	end
+2:	begin
+	phase_word[bits] <= phase_data;			// this is the second CBCLK after negedge of CLRCLK
+		if (bits == 0)CC_state <= 0; 		// done so restart
+		else begin
+		bits <= bits - 1'b1;
+		CC_state <= 2;  
+		end
+	end
+default: CC_state <= 0;
+endcase
+end
+
+// sync AD9912 phase data to 48kHz LR clock
+reg [31:0]AD9912_phase; 
+always @ (negedge CLRCLK)  
+begin 
+	AD9912_phase <= phase_word[31:0];
+end
 
 
 //////////////////////////////////////////////////////////////
