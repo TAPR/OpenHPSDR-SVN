7e663f3265f3e04d3291f44fd0355ecaad083654
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2008-12-24 04:15:50 +0000

    Added remarks

--- USBBlaster-Binaries/usbblaster.bat
+++ USBBlaster-Binaries/usbblaster.bat
@@ -1,3 +1,6 @@
+rem
+rem this loads code into the FX2 to make it look like an Altera USBblaster
+rem
 load_firmwareV1.1.exe  0xfffe  0x7 ozyfw-sdr1k.hex
 sleep 1000
 upload_fpgaV1.1.exe 0xfffe 0x7 usb_blaster.rbf
