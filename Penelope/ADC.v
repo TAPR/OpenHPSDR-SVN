aff2a7ab39e9a2b0746258a918d604d699ccb215
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-01-01 11:44:42 +0000

    Latest version - compiles with Quartus V8.1

--- Penelope/ADC.v
+++ Penelope/ADC.v
@@ -30,74 +30,73 @@
 
 module ADC(clock, SCLK, nCS, MISO, MOSI, AIN5);
 
-output  nCS;
-input  clock;
-input  MISO;
-output MOSI;
-output SCLK;
-output [11:0]AIN5;
+input  wire       clock;
+output reg        SCLK;
+output reg        nCS;
+input  wire       MISO;
+output reg        MOSI;
+output reg [11:0] AIN5; // reg holds VFWD volts
 
 wire [15:0] data;
-reg  [2:0]ADC;
-reg  [3:0]bit_cnt;
-reg  nCS;
-reg  SCLK;
-reg  MOSI;
-reg  [11:0] AIN5; 		// reg holds VFWD volts
-reg [11:0] temp_AIN5;	// 
+reg   [2:0] ADC_state;
+reg   [3:0] bit_cnt;
+reg  [11:0] temp_AIN5;	// 
 
 assign data = 16'b0010_0000_0000_0000; // address of AIN5
 
 // NOTE: this code generates the SCLK clock for the ADC
+always @ (posedge clock)
+begin
+  case (ADC_state)
+  0:
+	begin
+    nCS       <= 1'b1;          // set nCS high
+    bit_cnt   <= 4'd15;         // set bit counter
+    ADC_state <= 1;
+	end
+	
+  1:
+	begin
+    nCS       <= 0;             // select ADC
+    MOSI      <= data[bit_cnt];	// shift data out to MOSI
+    ADC_state <= 2;
+	end
+	
+  2:
+	begin
+    SCLK      <= 1'b1;          // SCLK high
+    ADC_state <= 3;
+	end
+	
+  3:
+	begin
+    SCLK      <= 1'b0;          // SCLK low
+    ADC_state <= 4;
+	end
+
+  4:
+	begin
+    if (bit_cnt == 0)           // restart ? - will change address here later on
+      ADC_state <= 0;
+    else
+    begin
+      bit_cnt   <= bit_cnt - 1'b1; // do all again
+      ADC_state <= 1;
+    end
+	end 
+	
+  default:
+    ADC_state <= 0;
+  endcase
+end 
 
 always @ (posedge clock)
 begin
-case (ADC)
-
-0:	begin
-	nCS <= 1'b1; 			// set nCS high
-	bit_cnt <= 4'd15; 		// set bit counter
-	ADC <= 1;
-	end
-	
-1:  begin
-	nCS <= 0; 				// select ADC
-	MOSI <= data[bit_cnt];	// shift data out to MOSI
-	ADC <= 2;
-	end
-	
-2:  begin
-	SCLK <= 1;				// SCLK high
-	ADC <= 3;
-	end
-	
-3:  begin
-	SCLK <= 0;				// SCLK low
-	ADC <= 4;
-	end
-
-4:  begin
-	if (bit_cnt == 0)begin 	// restart ? - will change address here later on
-		ADC <= 0;
-	end
-	else begin
-		bit_cnt <= bit_cnt - 1'b1; // do all again
-		ADC <= 1;
-	end
-	end 
-	
-default: ADC <= 0;
-endcase
-end 
-
-// SPI receiver - doesn't get much simpler than this!
-
-always @ (negedge SCLK)
-begin
-if (bit_cnt > 11)				// start capturing data at bit counter = 11
-	AIN5 <= temp_AIN5;			// latch data when not shifting
-else 
-	temp_AIN5[bit_cnt] <= MISO; // capture incoming data
+  if (ADC_state == 0)
+    AIN5 <= temp_AIN5;			    // latch data when not shifting
+ 
+  if (SCLK && (bit_cnt <= 11))  // start capturing data at bit counter = 11
+    temp_AIN5[bit_cnt] <= MISO; // capture incoming data
 end 
 
 	
--- Penelope/ALC.v
+++ Penelope/ALC.v
@@ -1,6 +1,6 @@
 // V1.0 17th July 2007
 //
-// Copyright 2007 Phil Harman VK6APH
+// Copyright 2008 Kirk Weedman KD7IRS
 //
 //  HPSDR - High Performance Software Defined Radio
 //
@@ -24,29 +24,21 @@
 
 
 
-// Fixed point multiplier. Sample is a 16 bit signed integer. Multiply is a 16 bit fraction [0].[16]
-// in the form  0.xxxx  e.g. to multiply Sample by  0.1234  use  1234 for multiply.
+// Fixed point signed multiplier. Multipier is assumed positive
 // Result is a 16 bit integer. 
-
-// Shift fractional part left by 16 bits and divide by power of 10 being used to represent fraction.
-// Need 16 bits for fraction so we can represent 0.9999  (i.e. 9999).
-
 module ALC (out, sample, multiply);
 
-input [15:0]sample;
-input [15:0]multiply;
-output wire [15:0]out;
-wire [15:0]fraction;
+output wire [15:0] out;
+input       [15:0] sample;
+input       [15:0] multiply;
+
 wire [47:0]result;
 wire [15:0] pos_sample;
 
 assign pos_sample = sample[15] ? (~sample + 1'b1) : sample;  // convert to positive if negative 
 
-assign fraction = {multiply, 16'd0} / 10000;  // [16].[16]/10000 = [0].[16]
+assign result = pos_sample * multiply ; // B0 * B16 scalar  (single 16 by 16 bit multiply)
 
-assign result = {pos_sample,16'd0} * fraction;   // [16].[16] * [0].[16] = [16].[32] = [47:0]
-
-assign out = sample[15] ? (~result[47:32] + 1) : result[47:32] ; // [15:0], revert to negative if needed
-
-
+assign out = sample[15] ? ~result[31:16]+16'b1 : result[31:16];  // or result >> 16
+ 
 endmodule
