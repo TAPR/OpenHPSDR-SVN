8fbd59697d7e68cf4a69bef01f4ff2cdd66d4862
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-02-25 12:00:33 +0000

    Added files to update Penelope flash memory

---
+++ USBBlaster-Binaries/Program-Penelope-EPCS4.bat
@@ -0,0 +1,38 @@
+@ECHO OFF
+CLS
+:: Load the FX2 code 
+load_firmwareV1.1.exe  0xfffe  0x7 ozyfw-sdr1k.hex
+ECHO.
+sleep 3000
+upload_fpgaV1.1.exe 0xfffe 0x7 usb_blaster.rbf
+ECHO.
+sleep 3000
+load_firmwareV1.1.exe  0xfffe  0x7 std.hex
+ECHO.
+sleep 3000
+:: prompt the user for the file to use
+:LOOP
+ECHO.     
+ECHO.      
+ECHO A. Program using Penelope_v1.1
+ECHO Q. Quit
+ECHO.      
+SET Choice=
+SET /P Choice=Type the letter and press Enter: 
+
+IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
+ECHO.
+:: /I makes the IF comparison case-insensitive
+IF /I '%Choice%'=='A' GOTO ItemA
+IF /I '%Choice%'=='Q' GOTO End
+ECHO "%Choice%" is not valid. Please try again.
+ECHO.
+GOTO Loop
+:ItemA
+c:\altera\81\qprogrammer\bin\quartus_pgm -c USB-Blaster Penelope_v1.1.cdf
+GOTO CONTINUE
+:CONTINUE
+PAUSE
+CLS
+:End
+
