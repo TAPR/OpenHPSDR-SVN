5222dfd3f214001561dc3716e7b07ea77cb59ccf
kd5tfd <kd5tfd@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-03-05 04:42:07 +0000

    Update to use native code utils, now supports Vista

--- USBBlaster-Binaries/usbblaster.bat
+++ USBBlaster-Binaries/usbblaster.bat
@@ -1,9 +1,10 @@
 rem
 rem this loads code into the FX2 to make it look like an Altera USBblaster
 rem
-load_firmwareV1.1.exe  0xfffe  0x7 ozyfw-sdr1k.hex
-sleep 1000
-upload_fpgaV1.1.exe 0xfffe 0x7 usb_blaster.rbf
-sleep 1000
-load_firmwareV1.1.exe  0xfffe  0x7 std.hex
+rem - Updated 4 March 2009 to use Vista capabile native code utils
+loadFW.exe  0xfffe  0x7 ozyfw-sdr1k.hex
+msecsleep 1000
+loadFPGA.exe 0xfffe 0x7 usb_blaster.rbf
+msecsleep 1000
+loadFW.exe  0xfffe  0x7 std.hex
 pause
