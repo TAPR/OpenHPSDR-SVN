5171ccba3463f45de7be21bc9530efc95d3879c8
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-03-09 06:23:33 +0000

    changed sleep to 3 seconds in usbblaster.bat since 1 second may be too short on some PCs.

--- trunk/USBBlaster-Binaries/usbblaster.bat
+++ trunk/USBBlaster-Binaries/usbblaster.bat
@@ -3,8 +3,8 @@ rem this loads code into the FX2 to make it look like an Altera USBblaster
 rem
 rem - Updated 4 March 2009 to use Vista capabile native code utils
 loadFW.exe  0xfffe  0x7 ozyfw-sdr1k.hex
-msecsleep 1000
+msecsleep 3000
 loadFPGA.exe 0xfffe 0x7 usb_blaster.rbf
-msecsleep 1000
+msecsleep 3000
 loadFW.exe  0xfffe  0x7 std.hex
 pause
