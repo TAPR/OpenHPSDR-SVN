1a9cc0ed816d2a14898a4404fd70d6272b72a96f
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-03-22 06:57:58 +0000

    Added option to select which version of Quartus Programmer code is being used. Updated README.txt.

--- USBBlaster-Binaries/Program-Penelope-EPCS4.bat
+++ USBBlaster-Binaries/Program-Penelope-EPCS4.bat
@@ -10,6 +10,34 @@ msecsleep 3000
 loadFW.exe  0xfffe  0x7 std.hex
 ECHO.
 msecsleep 3000
+:: prompt the user for the version of Quartus to use
+:LOOP1
+ECHO Which version of the Quartus Programmer are you using?
+ECHO.
+ECHO.
+ECHO  1. Quartus V8.1
+ECHO  2. Quartus V9.0
+ECHO  3. Quit
+ECHO.
+SET Choice=
+SET /P Choice=Type the number and press Enter:
+IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
+ECHO.
+:: /I makes the IF comparison case-insensitive
+IF /I '%Choice%'=='1' GOTO Q81
+IF /I '%Choice%'=='2' GOTO Q90
+IF /I '%Choice%'=='Q' GOTO End
+ECHO "%Choice%" is not valid. Please try again.
+ECHO.
+GOTO Loop1
+
+:Q81
+SET DIRECTORY=c:\altera\81\qprogrammer\bin\quartus_pgm
+GOTO LOOP
+
+:Q90
+SET DIRECTORY=c:\altera\90\qprogrammer\bin\quartus_pgm
+GOTO LOOP
 :: prompt the user for the file to use
 :LOOP
 ECHO.     
@@ -29,7 +57,7 @@ ECHO "%Choice%" is not valid. Please try again.
 ECHO.
 GOTO Loop
 :ItemA
-c:\altera\81\qprogrammer\bin\quartus_pgm -c USB-Blaster Penelope_v1.1.cdf
+%DIRECTORY% -c USB-Blaster Penelope_v1.1.cdf
 GOTO CONTINUE
 :CONTINUE
 PAUSE
