d99d0a1e2a0806e995d5de66035b11a959da1c12
snance <snance@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-05-06 15:46:07 +0000

    1.7.4 progressive

--- trunk/K5FR/DDUtil/Source/Properties/AssemblyInfo.cs
+++ trunk/K5FR/DDUtil/Source/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.7.3.9")]
+[assembly: AssemblyFileVersion("1.7.4.00")]
