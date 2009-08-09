c136c2b9689adfeac1925ea62fe373091506fd11
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-08-09 06:40:25 +0000

    V1.5 of Ozy_Janus loaded

---
+++ trunk/Ozy V1.5/OzyJanus/Rx_fifo_inst.v
@@ -0,0 +1,11 @@
+Rx_fifo	Rx_fifo_inst (
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.rdreq ( rdreq_sig ),
+	.sclr ( sclr_sig ),
+	.wrreq ( wrreq_sig ),
+	.empty ( empty_sig ),
+	.full ( full_sig ),
+	.q ( q_sig ),
+	.usedw ( usedw_sig )
+	);
