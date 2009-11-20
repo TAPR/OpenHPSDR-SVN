39c3ce19b643ece56f896415b35313b0609c5bff
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-11-20 23:44:48 +0000

    Added Quartus V9.1 suppport

--- USBBlaster-Binaries/Program-Janus-EPM240T100.bat
+++ USBBlaster-Binaries/Program-Janus-EPM240T100.bat
@@ -19,6 +19,7 @@ ECHO  1. Quartus V8.1
 ECHO  2. Quartus V9.0
 ECHO  3. Quartus V9.0 sp1
 ECHO  4. Quartus V9.0 sp2
+ECHO  5. Quartus V9.1
 ECHO  Q. Quit
 ECHO.
 SET Choice=
@@ -30,6 +31,7 @@ IF /I '%Choice%'=='1' GOTO Q81
 IF /I '%Choice%'=='2' GOTO Q90
 IF /I '%Choice%'=='3' GOTO Q90sp1
 IF /I '%Choice%'=='4' GOTO Q90sp2
+IF /I '%Choice%'=='5' GOTO Q91
 IF /I '%Choice%'=='Q' GOTO End
 ECHO "%Choice%" is not valid. Please try again.
 ECHO.
@@ -51,6 +53,9 @@ GOTO LOOP
 SET DIRECTORY=C:\altera\90sp2\qprogrammer\bin\quartus_pgm
 GOTO LOOP
 
+:Q91
+SET DIRECTORY=C:\altera\91\qprogrammer\bin\quartus_pgm
+GOTO LOOP
 
 :: prompt the user for the file to use
 :LOOP
