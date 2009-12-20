36237cd58c86f326697e575eed4adb8f66346307
kd5tfd <kd5tfd@fab57ed4-c20f-0410-8556-bd0ba87add53>
2009-12-20 03:21:01 +0000

    Delete Obsolete code -- archived to openhpsdr.org

--- trunk/KD5TFD/PowerSDR/3in4out-48khz/bin/Release/PostBuildEvent.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-copy "F:\sdr1000.src\3i4o-48khz\Source\Console\CAT\CATStructs.xml" "..\..\bin\Release\"; 
-copy "F:\sdr1000.src\3i4o-48khz\Source\Console\..\FFTW\libfftw3f-3.dll" "..\..\bin\Release\"
-if errorlevel 1 goto CSharpReportError
-goto CSharpEnd
-:CSharpReportError
-echo Project error: A tool returned an error code from the build event
-exit 1
-:CSharpEnd
