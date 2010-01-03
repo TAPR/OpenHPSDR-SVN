6f956e0107aaf0c0c343be08e6085d01fe86a05e
AE6VK <AE6VK@fab57ed4-c20f-0410-8556-bd0ba87add53>
2010-01-03 22:09:18 +0000

    From http://openhpsdr.org/svn_archive/N8VB/OZY_V1.zip.

---
+++ AE6VK/HPSDR Sources 03-Jan-2010/Ozy-USB-UtilsV2.0 (from archive)/src/read_I2CSPEED/read_I2CSPEED/Program.cs
@@ -0,0 +1,97 @@
+/*
+ * HPSDR/OZY - High Performance Software Defined Radio, OZY_USB_LIB_V1
+ *
+ * Copyright (C) 2006 Philip A. Covington, N8VB
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+// This program is a command line utility to read the I2C Speed on the FX2.
+// It finds the FX2 by VID and PID
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HPSDR_USB_LIB_V1;
+using System.Globalization;
+
+namespace read_I2CSPEED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ((args.Length != 2) || (args.Length == 0))
+            {
+                Console.WriteLine("usage: read_EEPROM <VID> <PID>");
+                return;
+            }
+
+            if (args[0].Length > 2)
+            {
+                if (args[0].Substring(0, 2) != "0x")
+                {
+                    Console.WriteLine("You must specify VID in Hex (0x0)");
+                    return;
+                }
+                else
+                {
+                    args[0] = args[0].Substring(2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You must specify VID in Hex (0x0)");
+                return;
+            }
+
+
+            int vid = int.Parse(args[0], NumberStyles.HexNumber);
+
+            if (args[1].Length > 2)
+            {
+                if (args[1].Substring(0, 2) != "0x")
+                {
+                    Console.WriteLine("You must specify PID in Hex (0x0)");
+                    return;
+                }
+                else
+                    args[1] = args[1].Substring(2);
+            }
+            else
+            {
+                Console.WriteLine("You must specify PID in Hex (0x0)");
+                return;
+            }
+
+            int pid = int.Parse(args[1], NumberStyles.HexNumber);
+                        
+            IntPtr usb_dev_handle = USB.InitFindAndOpenDevice(vid, pid);
+
+            if ((OZY.Read_I2C_Speed(usb_dev_handle) == 1))
+            {
+                Console.WriteLine("I2C speed is 400kHz");
+            }
+            else
+            {
+                Console.WriteLine("I2C speed is 100kHz");
+            }
+            
+            Console.WriteLine("Closing device...");
+            libUSB_Interface.usb_close(usb_dev_handle);
+            Console.WriteLine("done...");
+        }
+    }
+}
