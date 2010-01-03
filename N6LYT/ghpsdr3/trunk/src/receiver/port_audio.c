b9dc8f7791d42b3799681c68b7dc74b519183009
John Melton <john.d.melton@googlemail.com>
2010-01-03 13:45:26 +0000

    fixed bug so can play audio locally using port audio

--- N6LYT/ghpsdr3/trunk/src/receiver/port_audio.c
+++ N6LYT/ghpsdr3/trunk/src/receiver/port_audio.c
@@ -4,13 +4,14 @@
 
 #include "portaudio.h"
 
+#define CHANNELS 2
 #define SAMPLE_RATE 48000
-#define SAMPLES_PER_BUFFER 1024
+#define SAMPLES_PER_BUFFER 2048
 
 static PaStream *stream;
 
 static int insert=0;
-static unsigned char audio_buffer[1024*sizeof(short)*2*2];
+static float audio_buffer[SAMPLES_PER_BUFFER*CHANNELS*2];
 static sem_t audio_sem;
 
 static int write_port_audio_thread_id;
@@ -26,7 +27,7 @@ static void write_port_audio_thread(void* arg) {
         }
 
         sem_wait(&audio_sem);
-        rc=Pa_WriteStream(stream,&audio_buffer[sizeof(audio_buffer)/2],SAMPLES_PER_BUFFER);
+        rc=Pa_WriteStream(stream,&audio_buffer[SAMPLES_PER_BUFFER],SAMPLES_PER_BUFFER);
         if(rc!=0) {
             fprintf(stderr,"writing second audio_buffer rc=%d",rc);
         }
@@ -44,7 +45,7 @@ void open_port_audio() {
         exit(1);
     }
 
-    rc=Pa_OpenDefaultStream(&stream,0,2,paInt16,SAMPLE_RATE,SAMPLES_PER_BUFFER,
+    rc=Pa_OpenDefaultStream(&stream,0,CHANNELS,paFloat32,SAMPLE_RATE,SAMPLES_PER_BUFFER,
                             NULL, NULL);
     if(rc!=paNoError) {
         fprintf(stderr,"Pa_OpenDefaultStream failed: %s\n",Pa_GetErrorText(rc));
@@ -74,25 +75,17 @@ void pa_close() {
 }
 
 int write_port_audio(float* left_samples,float* right_samples,int samples,int increment) {
-    int rc;
-    int bytes;
     int i;
-    short sample;
 
     for(i=0;i<samples;i+=increment) {
-        sample=(short)(left_samples[i]*32767.0F);
-        audio_buffer[insert++]=sample>>8;
-        audio_buffer[insert++]=sample;
-        sample=(short)(right_samples[i]*32767.0F);
-        audio_buffer[insert++]=sample>>8;
-        audio_buffer[insert++]=sample;
-    }
-
-    if(insert==sizeof(audio_buffer)/2) {
-        sem_post(&audio_sem);
-    } else if(insert==sizeof(audio_buffer)) {
-        insert=0;
-        sem_post(&audio_sem);
+        audio_buffer[insert++]=left_samples[i];
+        audio_buffer[insert++]=right_samples[i];
+        if(insert==SAMPLES_PER_BUFFER*2) {
+            sem_post(&audio_sem);
+        } else if(insert==(SAMPLES_PER_BUFFER*4)) {
+            insert=0;
+            sem_post(&audio_sem);
+        }
     }
 
 }
