15f2c07215cf1ff10a5d58f5083510a60185d393
John Melton <john.d.melton@googlemail.com>
2010-02-17 20:13:59 +0000

    expanded menus for android

--- N6LYT/ghpsdr3/branches/android/JMonitor/src/org/g0orx/JMonitor.java
+++ N6LYT/ghpsdr3/branches/android/JMonitor/src/org/g0orx/JMonitor.java
@@ -18,7 +18,7 @@ public class JMonitor extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        connection=new Connection("81.146.61.118",8000);
+        connection=new Connection(server,BASE_PORT+receiver);
         
         update=new Update(connection);
         
@@ -39,6 +39,7 @@ public class JMonitor extends Activity {
         connection.setMode(0);
         connection.setFilter(-2850,-150);
         connection.setGain(30);
+        connection.setAGC(AGC_LONG);
     }
     
     public void onRestart() {
@@ -113,6 +114,7 @@ public class JMonitor extends Activity {
     	Dialog dialog;
     	AlertDialog.Builder builder;
     	
+    	dialog=null;
     	switch(id) {
     	    case MENU_BAND:
     	    	builder=new AlertDialog.Builder(this);
@@ -244,19 +246,345 @@ public class JMonitor extends Activity {
     		    break;
     	    case MENU_FILTER:
     	    	builder=new AlertDialog.Builder(this);
-    	    	builder.setTitle("Select a Band");
-    	    	builder.setSingleChoiceItems(bands,-1,new DialogInterface.OnClickListener() {
-    	    		public void onClick(DialogInterface dialog,int item) {
-    	    			//
-    	    			dialog.dismiss();
-    	    		}
-    	    	});
-    	    	dialog=builder.create();
+    	    	builder.setTitle("Select Filter");
+    	    	CharSequence[] filters=null;
+    	    	switch(connection.getMode()) {
+    	    	case MODE_LSB:
+				case MODE_USB:
+				case MODE_DSB:
+					filters=ssbFilters;
+					break;
+				case MODE_CWL:
+				case MODE_CWU:
+					filters=cwFilters;
+					break;
+				case MODE_FMN:
+				case MODE_AM:
+				case MODE_DIGU:
+				case MODE_DIGL:
+				case MODE_SAM:
+					filters=amFilters;
+					break;
+				case MODE_SPEC:
+				case MODE_DRM:
+					filters=null;
+					break;
+    	    	}
+    	    	if(filters!=null) {
+    	    	    builder.setSingleChoiceItems(ssbFilters,FILTER_5,new DialogInterface.OnClickListener() {
+    	    		    public void onClick(DialogInterface dialog,int item) {
+    	    			    switch(item) {
+    	    			    case FILTER_0:
+    	    				    switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-5150,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,5150);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(5000,5000);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-500,-cwPitch+500);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-500,cwPitch+500);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-8000,8000);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_1:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-4550,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,4550);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-4400,4400);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-400,-cwPitch+400);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-400,cwPitch+400);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-6000,6000);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_2:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-3950,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,3950);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-3800,3800);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-375,-cwPitch+375);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-375,cwPitch+375);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-5000,5000);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_3:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-3450,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,3450);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-3300,3300);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-300,-cwPitch+300);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-300,cwPitch+300);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-4000,4000);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_4:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-3050,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,3050);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-2900,2900);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-250,-cwPitch+250);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-250,cwPitch+250);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-3300,3300);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_5:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-2850,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,2850);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-2700,2700);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-200,-cwPitch+200);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-200,cwPitch+200);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-2600,2600);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_6:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-2550,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,2550);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-2400,2400);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-125,-cwPitch+125);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-125,cwPitch+125);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-2000,2000);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_7:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-2250,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,2250);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-2100,2100);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-50,-cwPitch+50);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-50,cwPitch+50);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-1550,1550);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_8:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-1950,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,1950);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-1800,1800);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-25,-cwPitch+25);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-25,cwPitch+25);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-1450,1450);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    case FILTER_9:
+    	    			    	switch(connection.getMode()) {
+    	    				    case MODE_LSB:
+    	    				    	connection.setFilter(-1150,-150);
+    	    					    break;
+    	    				    case MODE_USB:
+    	    				    	connection.setFilter(150,1150);
+    	    					    break;
+    	    				    case MODE_DSB:
+    	    				    	connection.setFilter(-1000,1000);
+    	    					    break;
+    	    				    case MODE_CWL:
+    	    				    	connection.setFilter(-cwPitch-12,-cwPitch+12);
+    	    					    break;
+    	    				    case MODE_CWU:
+    	    				    	connection.setFilter(cwPitch-12,cwPitch+12);
+    	    					    break;
+    	    				    case MODE_FMN:
+    	    				    case MODE_AM:
+    	    				    case MODE_DIGU:
+    	    				    case MODE_DIGL:
+    	    				    case MODE_SAM:
+    	    				    	connection.setFilter(-1000,1000);
+    	    					    break;
+    	    					case MODE_SPEC:
+    	    					    break;
+    	    				    case MODE_DRM:
+    	    					    break;
+    	    				    }
+    	    				    break;
+    	    			    }
+    	    		    	dialog.dismiss();
+    	    		    }
+    	    	    });
+    	    	    dialog=builder.create();
+    	    	}
     		    break;
     	    case MENU_AGC:
     	    	builder=new AlertDialog.Builder(this);
-    	    	builder.setTitle("Select a Band");
-    	    	builder.setSingleChoiceItems(bands,-1,new DialogInterface.OnClickListener() {
+    	    	builder.setTitle("Select AGC");
+    	    	builder.setSingleChoiceItems(agcs,agc,new DialogInterface.OnClickListener() {
     	    		public void onClick(DialogInterface dialog,int item) {
     	    			//
     	    			dialog.dismiss();
@@ -266,10 +594,22 @@ public class JMonitor extends Activity {
     		    break;
     	    case MENU_DSP:
     	    	builder=new AlertDialog.Builder(this);
-    	    	builder.setTitle("Select a Band");
-    	    	builder.setSingleChoiceItems(bands,-1,new DialogInterface.OnClickListener() {
-    	    		public void onClick(DialogInterface dialog,int item) {
+    	    	builder.setTitle("Select DSP");
+    	    	builder.setMultiChoiceItems(dsps,dsp_state,new DialogInterface.OnMultiChoiceClickListener() {
+    	    		public void onClick(DialogInterface dialog,int item,boolean state) {
     	    			//
+    	    			switch(item) {
+    	    			case DSP_NR:
+    	    				connection.setNR(state);
+    	    				break;
+    	    			case DSP_ANF:
+    	    				connection.setANF(state);
+    	    				break;
+    	    			case DSP_NB:
+    	    				connection.setNB(state);
+    	    				break;
+    	    			}
+    	    			
     	    			dialog.dismiss();
     	    		}
     	    	});
@@ -277,10 +617,10 @@ public class JMonitor extends Activity {
     		    break;
     	    case MENU_GAIN:
     	    	builder=new AlertDialog.Builder(this);
-    	    	builder.setTitle("Select a Band");
-    	    	builder.setSingleChoiceItems(bands,-1,new DialogInterface.OnClickListener() {
+    	    	builder.setTitle("Select Gain");
+    	    	builder.setSingleChoiceItems(gains,GAIN_30,new DialogInterface.OnClickListener() {
     	    		public void onClick(DialogInterface dialog,int item) {
-    	    			//
+    	    			connection.setGain(item*10);
     	    			dialog.dismiss();
     	    		}
     	    	});
@@ -306,7 +646,6 @@ public class JMonitor extends Activity {
     public static final int MENU_GAIN=6;
     
     public static final CharSequence[] bands={"160","80","60","40","30","20","17","15","12","10","6","GEN","WWV"};
-    public static final CharSequence[] modes={"LSB","USB","DSB","CWL","CWU","FMN","AM","DIGU","SPEC","DIGL","SAM","DRM"};
     
     private int band=BAND_40;
     
@@ -324,6 +663,8 @@ public class JMonitor extends Activity {
     public static final int BAND_GEN=11;
     public static final int BAND_WWV=12;
     
+    public static final CharSequence[] modes={"LSB","USB","DSB","CWL","CWU","FMN","AM","DIGU","SPEC","DIGL","SAM","DRM"};
+    
     public static final int MODE_LSB=0;
     public static final int MODE_USB=1;
     public static final int MODE_DSB=2;
@@ -337,5 +678,62 @@ public class JMonitor extends Activity {
     public static final int MODE_SAM=10;
     public static final int MODE_DRM=11;
     
+    public static final CharSequence[] agcs={"OFF","LONG","SLOW","MEDIUM","FAST"};
+    private int agc=AGC_LONG;
     
+    public static final int AGC_OFF=0;
+    public static final int AGC_LONG=1;
+    public static final int AGC_SLOW=2;
+    public static final int AGC_MEDIUM=3;
+    public static final int AGC_FAST=4;
+    
+    public static final CharSequence[] dsps={"NR","ANF","NB"};
+    
+    public static final int DSP_NR=0;
+    public static final int DSP_ANF=1;
+    public static final int DSP_NB=2;
+    
+    private boolean[] dsp_state={false,false,false};
+    
+    public static final CharSequence[] gains={"0","10","20","30","40","50","60","70","80","90","100"};
+    
+    public int gain=30;
+    
+    public static final int GAIN_0=0;
+    public static final int GAIN_10=1;
+    public static final int GAIN_20=2;
+    public static final int GAIN_30=3;
+    public static final int GAIN_40=4;
+    public static final int GAIN_50=5;
+    public static final int GAIN_60=6;
+    public static final int GAIN_70=7;
+    public static final int GAIN_80=8;
+    public static final int GAIN_90=9;
+    public static final int GAIN_100=10;
+    
+    public static final CharSequence[] ssbFilters={"5.0k","4.4k","3.8k","3.3k","2.9k","2.7k","2.4k","2.1k","1.8k","1.0k"};
+    public static final CharSequence[] cwFilters={"1.0k","800","750","600","500","400","250","100","50","25"};
+    public static final CharSequence[] amFilters={"16.0k","12.0k","10.0k","8.0k","6.6k","5.2k","4.0k","3.1k","2.9k","2.0k"};
+
+    private int filter=FILTER_5;
+    
+    public static final int FILTER_0=0;
+    public static final int FILTER_1=1;
+    public static final int FILTER_2=2;
+    public static final int FILTER_3=3;
+    public static final int FILTER_4=4;
+    public static final int FILTER_5=5;
+    public static final int FILTER_6=6;
+    public static final int FILTER_7=7;
+    public static final int FILTER_8=8;
+    public static final int FILTER_9=9;
+    
+    private int cwPitch=400;
+    
+    private String server="81.146.61.118";
+    private static final int BASE_PORT=8000;
+    private int port=8000;
+    private int receiver=0;
+    
+   
 }
