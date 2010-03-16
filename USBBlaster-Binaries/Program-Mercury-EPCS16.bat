588260c38e30133add2653df51ce22ebf8584cc5
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2010-03-16 10:57:34 +0000

    Added support for V9.1sp1 programmer software

--- USBBlaster-Binaries/Program-Mercury-EPCS16.bat
+++ USBBlaster-Binaries/Program-Mercury-EPCS16.bat
@@ -20,6 +20,7 @@ ECHO  2. Quartus V9.0
 ECHO  3. Quartus V9.0sp1
 ECHO  4. Quartus V9.0sp2
 ECHO  5. Quartus V9.1
+ECHO  6. Quartus V9.1sp1
 ECHO  Q. Quit
 ECHO.
 SET Choice=
@@ -32,6 +33,7 @@ IF /I '%Choice%'=='2' GOTO Q90
 IF /I '%Choice%'=='3' GOTO Q90sp1
 IF /I '%Choice%'=='4' GOTO Q90sp2
 IF /I '%Choice%'=='5' GOTO Q91
+IF /I '%Choice%'=='6' GOTO Q91sp1
 IF /I '%Choice%'=='Q' GOTO End
 ECHO "%Choice%" is not valid. Please try again.
 ECHO.
@@ -58,7 +60,9 @@ GOTO LOOP
 SET DIRECTORY=c:\altera\91\qprogrammer\bin\quartus_pgm
 GOTO LOOP
 
-
+:Q91sp1
+SET DIRECTORY=c:\altera\91sp1\qprogrammer\bin\quartus_pgm
+GOTO LOOP
 
 :: prompt the user for the file to use
 :LOOP
