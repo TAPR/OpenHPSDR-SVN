85124133237220d623fc996bed3a84bc1cff6135
snance <snance@fab57ed4-c20f-0410-8556-bd0ba87add53>
2010-05-02 19:33:43 +0000

    1.8.5.12 - Added DDWW macro and integration to WinWarbler macros (uses DDE). - Revised Icom PW-1 code to handle partial message errors. Tnx to Basil W6BT for his help with this defect. - Added LED indicators for tuning knob to main and mini forms.

--- trunk/K5FR/DDUtil/Source/DDUtil/Properties/AssemblyInfo.cs
+++ trunk/K5FR/DDUtil/Source/DDUtil/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.8.5.10")]
+[assembly: AssemblyFileVersion("1.8.5.12")]
