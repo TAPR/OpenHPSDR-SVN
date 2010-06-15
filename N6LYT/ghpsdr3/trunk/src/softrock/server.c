aefaccf2cec16f3fca44202fe712c9d0f72b5f2e
John Melton <john.d.melton@googlemail.com>
2010-06-15 12:22:08 +0000

    added si570 control for Softrock

--- N6LYT/ghpsdr3/trunk/src/softrock/server.c
+++ N6LYT/ghpsdr3/trunk/src/softrock/server.c
@@ -36,6 +36,8 @@
 #include <string.h>
 #include <getopt.h>
 
+#include <usb.h>
+
 #include "client.h"
 #include "listener.h"
 #include "softrock.h"
@@ -47,19 +49,46 @@ static struct option long_options[] = {
     {"input",required_argument, 0, 2},
     {"output",required_argument, 0, 3},
     {"iq",no_argument, 0, 4},
-    {"qi",no_argument, 0, 5}
+    {"qi",no_argument, 0, 5},
+    {"si570",no_argument, 0, 6},
+    {"verbose",no_argument, 0, 7},
+    {"startupfreq",required_argument, 0, 8},
+    {"multiplier",required_argument, 0, 9},
+    {"correctedfreq",required_argument, 0, 10},
+    {"serialid",required_argument, 0, 11}
 };
 
 static char* short_options="sd";
 static int option_index;
 
+int si570=0;
+int verbose=0;
+double startupFreq=56.32;
+double multiplier=4;
+int i2cAddress = 0x55;
+double fXtall = 114.285;
+char* usbSerialID=NULL;
+
+usb_dev_handle* handle = NULL;
+
 void process_args(int argc,char* argv[]);
 
 int main(int argc,char* argv[]) {
 
+    int rc;
+
     softrock_set_device("/dev/dsp");
 
     process_args(argc,argv);
+
+    if(si570) {
+        usb_init();
+        rc=usbOpenDevice(&handle,0x16C0,"www.obdev.at",0x05DC,"DG8SAQ-I2C",usbSerialID);
+        if(rc!=0) {
+            fprintf(stderr,"Cannot open USB device\n");
+            exit(1);
+        }
+    }
 
     init_receivers();
 
@@ -103,6 +132,24 @@ fprintf(stderr,"process_args: output=%s\n",optarg);
                 break;
             case 5: // qi
                 softrock_set_iq(0);
+                break;
+            case 6: // si570
+                si570=1;
+                break;
+            case 7: // verbose
+                verbose=1;
+                break;
+            case 8: // startupfreq
+                startupFreq=atof(optarg);
+                break;
+            case 9: // multiplier
+                multiplier=atof(optarg);
+                break;
+            case 10: // corrected xtal freq
+                fXtall=atof(optarg);
+                break;
+            case 11: // serial ID
+                usbSerialID=optarg;
                 break;
             default:
                 fprintf(stderr,"invalid argument\n");
