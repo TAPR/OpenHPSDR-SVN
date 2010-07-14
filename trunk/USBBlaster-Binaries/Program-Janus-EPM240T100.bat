74554cb4a7e66df61379a24707781ca7050c8e8c
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2010-07-14 10:23:52 +0000

    Added support for Quartus V10.0

--- trunk/USBBlaster-Binaries/Program-Janus-EPM240T100.bat
+++ trunk/USBBlaster-Binaries/Program-Janus-EPM240T100.bat
@@ -21,6 +21,8 @@ ECHO  3. Quartus V9.0 sp1
 ECHO  4. Quartus V9.0 sp2
 ECHO  5. Quartus V9.1
 ECHO  6. Quartus V9.1sp1
+ECHO  7. Quartus V9.1sp2
+ECHO  8. Quartus 10.0
 ECHO  Q. Quit
 ECHO.
 SET Choice=
@@ -35,6 +37,7 @@ IF /I '%Choice%'=='4' GOTO Q90sp2
 IF /I '%Choice%'=='5' GOTO Q91
 IF /I '%Choice%'=='6' GOTO Q91sp1
 IF /I '%Choice%'=='7' GOTO Q91sp2
+IF /I '%Choice%'=='8' GOTO Q100
 IF /I '%Choice%'=='Q' GOTO End
 ECHO "%Choice%" is not valid. Please try again.
 ECHO.
@@ -66,6 +69,10 @@ GOTO LOOP
 
 :Q91sp2
 SET DIRECTORY=c:\altera\91sp2\qprogrammer\bin\quartus_pgm
+GOTO LOOP
+
+:Q100
+SET DIRECTORY=c:\altera\10.0\qprogrammer\bin\quartus_pgm
 GOTO LOOP
 
 :: prompt the user for the file to use
