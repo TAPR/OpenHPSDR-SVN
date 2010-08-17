ab7af599b295f4de72c1c02d5a87787792dcdbc0
jmelton <jmelton@fab57ed4-c20f-0410-8556-bd0ba87add53>
2010-08-17 07:18:44 +0000

    added Configure files

---
+++ trunk/N6LYT/ghpsdr3/trunk/src/QtRadio/Configure.cpp
@@ -0,0 +1,15 @@
+/*
+ * File:   Configure.cpp
+ * Author: john
+ *
+ * Created on 16 August 2010, 20:03
+ */
+
+#include "Configure.h"
+
+Configure::Configure() {
+    widget.setupUi(this);
+}
+
+Configure::~Configure() {
+}
