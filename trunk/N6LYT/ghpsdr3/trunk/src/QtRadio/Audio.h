10d244f3810c2d10c2c5656445e0f11a2fd8676c
jmelton <jmelton@fab57ed4-c20f-0410-8556-bd0ba87add53>
2010-08-31 11:47:14 +0000

    fixed problems with running at 8000 samples per second on Mac OS X by setting frequency to 8011 samples per second

--- trunk/N6LYT/ghpsdr3/trunk/src/QtRadio/Audio.h
+++ trunk/N6LYT/ghpsdr3/trunk/src/QtRadio/Audio.h
@@ -18,6 +18,10 @@
 
 #define BIGENDIAN
 
+// There are problems running at 8000 samples per second on Mac OS X
+// The resolution is to run at 8011 samples persecond.
+#define SAMPLE_RATE_FUDGE 11
+
 class Audio : public QObject {
     Q_OBJECT
 public:
