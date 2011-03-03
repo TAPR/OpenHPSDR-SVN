319ce61ed7cee5f4f8e1225bae8604dbb5cea322
John Melton <john.d.melton@googlemail.com>
2011-03-03 08:40:37 +0000

    updated version

--- N6LYT/ghpsdr3/trunk/src/HPSDRProgrammer/Version.h
+++ N6LYT/ghpsdr3/trunk/src/HPSDRProgrammer/Version.h
@@ -1,6 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "(ver 1.0)"
+#define VERSION "(ver 1.1)"
+
+// Version 1.0 initial release
+// Version 1.1 fixes from Berndt VK5ABN for Windows
 
 #endif // VERSION_H
