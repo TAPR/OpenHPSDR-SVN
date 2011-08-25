2dc0c08b96077dbca3dabbff70ee7f44fca608a2
jmelton <jmelton@fab57ed4-c20f-0410-8556-bd0ba87add53>
2011-08-25 07:52:37 +0000

    updated to latest header format

--- trunk/N6LYT/ghpsdr3/branches/java/jmonitor/src/jmonitor/Main.java
+++ trunk/N6LYT/ghpsdr3/branches/java/jmonitor/src/jmonitor/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        String server="192.168.1.58";
+        String server="g0orx.dyndns.org";
         int receiver=0;
         int limit=0;
         int i=0;
