fb986b63620bcd4a64c079b03e47289222152e4e
John Melton <john.d.melton@googlemail.com>
2011-10-11 06:33:27 +0000

    updated version number for Hermes changes

--- N6LYT/ghpsdr3/trunk/src/HPSDRProgrammer/Version.h
+++ N6LYT/ghpsdr3/trunk/src/HPSDRProgrammer/Version.h
@@ -1,12 +1,13 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "(ver 1.4)"
+#define VERSION "(ver 1.5)"
 
 // Version 1.0 initial release
 // Version 1.1 fixes from Berndt VK5ABN for Windows
 // Version 1.2 fixes timing problem with discovery on Windows
 // Version 1.3 removed references to pcap from Interfaces
 // Version 1.4 rewrite of discovery code
+// Version 1.5 Hermes changes
 
 #endif // VERSION_H
