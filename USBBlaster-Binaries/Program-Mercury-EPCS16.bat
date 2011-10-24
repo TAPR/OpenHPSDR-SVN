a17d3fd2bc04a2599bc0bf6e44b16a501bc5282a
pvharman <pvharman@fab57ed4-c20f-0410-8556-bd0ba87add53>
2011-10-24 10:15:46 +0000

    Added support for Mercury_EU and Quartus V11.0 sp1

--- USBBlaster-Binaries/Program-Mercury-EPCS16.bat
+++ USBBlaster-Binaries/Program-Mercury-EPCS16.bat
@@ -23,8 +23,8 @@ ECHO  5. Quartus V9.1
 ECHO  6. Quartus V9.1sp1
 ECHO  7. Quartus V9.1sp2
 ECHO  8. Quartus 10.0
-ECHO  9. Quartus V10.1
-ECHO  A. Quartus V10.1sp1
+ECHO  9. Quartus V10.1sp1
+ECHO  A. Quartus V11.0sp1
 ECHO  Q. Quit
 ECHO.
 SET Choice=
@@ -40,8 +40,8 @@ IF /I '%Choice%'=='5' GOTO Q91
 IF /I '%Choice%'=='6' GOTO Q91sp1
 IF /I '%Choice%'=='7' GOTO Q91sp2
 IF /I '%Choice%'=='8' GOTO Q100
-IF /I '%Choice%'=='9' GOTO Q101
-IF /I '%Choice%'=='A' GOTO Q101sp1
+IF /I '%Choice%'=='9' GOTO Q101sp1
+IF /I '%Choice%'=='A' GOTO Q110sp1
 IF /I '%Choice%'=='Q' GOTO End
 ECHO "%Choice%" is not valid. Please try again.
 ECHO.
@@ -74,6 +74,7 @@ SET DIRECTORY=c:\altera\91sp1\qprogrammer\bin\quartus_pgm
 GOTO LOOP
 
 :Q91sp2
+
 SET DIRECTORY=c:\altera\91sp2\qprogrammer\bin\quartus_pgm
 GOTO LOOP
 
@@ -82,12 +83,12 @@ SET DIRECTORY=c:\altera\10.0\qprogrammer\bin\quartus_pgm
 GOTO LOOP
 
 
-:Q101
-SET DIRECTORY=c:\altera\10.1\qprogrammer\bin\quartus_pgm
-GOTO LOOP
-
 :Q101sp1
 SET DIRECTORY=c:\altera\10.1sp1\qprogrammer\bin\quartus_pgm
+GOTO LOOP
+
+:Q110sp1
+SET DIRECTORY=c:\altera\11.0sp1\qprogrammer\bin\quartus_pgm
 GOTO LOOP
 
 :: prompt the user for the file to use
@@ -106,6 +107,7 @@ ECHO I. Program using Mercury_v2.8
 ECHO J. Program using Mercury_v2.9a
 ECHO K. Program using Mercury_Apollo_Test ** not for general use!
 ECHO L. Program using Mercury_v3.0
+ECHO Leu. Program using Mercury_eu_V3.0
 ECHO.
 ECHO Z. Mercury Test (carrier at 7.68MHz)
 ECHO Q. Quit
@@ -113,7 +115,9 @@ ECHO.
 SET Choice=
 SET /P Choice=Type the letter and press Enter:
 
-IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
+REM IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
+IF NOT '%Choice%'=='' SET Choice=%Choice:~0%
+
 ECHO.
 :: /I makes the IF comparison case-insensitive
 IF /I '%Choice%'=='A' GOTO ItemA
@@ -128,6 +132,7 @@ IF /I '%Choice%'=='I' GOTO ItemI
 IF /I '%Choice%'=='J' GOTO ItemJ
 IF /I '%Choice%'=='K' GOTO ItemK
 IF /I '%Choice%'=='L' GOTO ItemL
+IF /I '%Choice%'=='Leu' GOTO ItemLEU
 IF /I '%Choice%'=='Z' GOTO ItemZ
 
 IF /I '%Choice%'=='Q' GOTO End
@@ -169,6 +174,9 @@ GOTO CONTINUE
 GOTO CONTINUE
 :ItemL
 %DIRECTORY% -c USB-Blaster mercury_v3.0.cdf
+GOTO CONTINUE
+:ItemLEU
+%DIRECTORY% -c USB-Blaster mercury_eu_v3.0.cdf
 GOTO CONTINUE
 :ItemZ
 %DIRECTORY% -c USB-Blaster mercury_test.cdf
