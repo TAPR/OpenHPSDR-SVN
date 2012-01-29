eb93b2b54cf6680299f1b18ddaac646c48fc3b12
w5wc <w5wc@users.noreply.github.com>
2012-01-29 01:01:34 +0000

    Implemented AGC and transmit compression techniques coded by Warren, NR0V.

--- W5WC/PowerSDR_HPSDR_2/bin/Release/initozy11.bat
+++ W5WC/PowerSDR_HPSDR_2/bin/Release/initozy11.bat
@@ -1,5 +1,6 @@
 loadFW 0xfffe 0x0007 ozyfw-sdr1k.hex
 msecsleep 3000
+rem Mercury 2.9a Penelope v1.3
 rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v18.rbf
 
 rem Mercury v2.9a Penelope v1.4
@@ -10,5 +11,8 @@ rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v20.rbf
 
 rem Mercury v3.1 Penelope v1.6
 loadFPGA 0xfffe 0x0007 Ozy_Janus.v21.rbf
+
+rem Mercury v3.2 Penelope v1.7
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v22.rbf
 
 
