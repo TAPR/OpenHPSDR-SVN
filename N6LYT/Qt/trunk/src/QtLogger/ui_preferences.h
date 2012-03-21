73d9fa9f3bf30d4e7b4bc1565df3d56a855b7be7
Dave Larsen <kv0s.dave@gmail.com>
2012-03-21 02:57:11 +0000

    fix addDialog.ui

--- N6LYT/Qt/trunk/src/QtLogger/ui_preferences.h
+++ N6LYT/Qt/trunk/src/QtLogger/ui_preferences.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'preferences.ui'
 **
-** Created: Mon Jan 30 17:14:50 2012
-**      by: Qt User Interface Compiler version 4.7.4
+** Created: Tue Mar 20 21:55:51 2012
+**      by: Qt User Interface Compiler version 4.8.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
