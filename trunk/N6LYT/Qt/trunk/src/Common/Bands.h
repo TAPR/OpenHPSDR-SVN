7103ea5f6a6d684b2252b667ddfa576a5abfbcb0
jmelton <jmelton@fab57ed4-c20f-0410-8556-bd0ba87add53>
2012-04-22 08:32:34 +0000

    added Common/Bands.h

---
+++ trunk/N6LYT/Qt/trunk/src/Common/Bands.h
@@ -0,0 +1,51 @@
+// ham bands
+#define HAM_BAND_160    0
+#define HAM_BAND_80     1
+#define HAM_BAND_60     2
+#define HAM_BAND_40     3
+#define HAM_BAND_30     4
+#define HAM_BAND_20     5
+#define HAM_BAND_17     6
+#define HAM_BAND_15     7
+#define HAM_BAND_12     8
+#define HAM_BAND_10     9
+#define HAM_BAND_6      10
+#define HAM_BAND_4      11
+#define HAM_BAND_144    12
+#define HAM_BAND_432    13
+#define HAM_BAND_1240   14
+#define HAM_BAND_2400   15
+
+#define HAM_BAND_LAST   15
+
+// broadcast bands
+#define BC_BAND_120     16
+#define BC_BAND_90      17
+#define BC_BAND_75      18
+#define BC_BAND_60      19
+#define BC_BAND_49      20
+#define BC_BAND_41      21
+#define BC_BAND_31      22
+#define BC_BAND_25      23
+#define BC_BAND_22      24
+#define BC_BAND_19      25
+#define BC_BAND_16      26
+#define BC_BAND_15      27
+#define BC_BAND_13      28
+#define BC_BAND_11      29
+
+// AIR band
+#define AIR_BAND        30
+
+// General Band
+#define GEN_BAND        31
+
+// WWV
+#define WWV_BAND_2_5    32
+#define WWV_BAND_5      33
+#define WWV_BAND_10     34
+#define WWV_BAND_15     35
+#define WWV_BAND_20     36
+
+// XVTR
+#define XVTR_BAND       37
