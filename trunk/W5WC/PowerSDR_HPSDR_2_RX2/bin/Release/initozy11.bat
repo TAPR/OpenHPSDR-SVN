4220bdf7b3f0742104a835bb3965641f3abbe51f
w5wc <w5wc@fab57ed4-c20f-0410-8556-bd0ba87add53>
2012-07-09 00:56:17 +0000

    Added updated Ozy_Janus.v22.rbf file. initozy11.bat now loads Ozy_Janus.v22.rbf

--- trunk/W5WC/PowerSDR_HPSDR_2_RX2/bin/Release/initozy11.bat
+++ trunk/W5WC/PowerSDR_HPSDR_2_RX2/bin/Release/initozy11.bat
@@ -1,7 +1,7 @@
 loadFW 0xfffe 0x0007 ozyfw-sdr1k.hex
 msecsleep 3000
 rem Mercury v3.1 Penelope v1.6
-loadFPGA 0xfffe 0x0007 Ozy_Janus.v21.rbf
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v21.rbf
 
 rem Mercury v3.2 Penelope v1.7
-rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v22.rbf
+loadFPGA 0xfffe 0x0007 Ozy_Janus.v22.rbf
