c3fa0da3a66a65fc0d0175cbddcd6756a8679c91
Tom McDermott, N5EG <Tom-McDermott@users.noreply.github.com>
2012-11-17 00:51:20 +0000

    Initial import of Gnuradio 3.6.2 module for Hermes/Metis. Rx only, 1 Rx only. Set sample rate, preamp and frequency.

---
+++ N5EG/gr-hpsdr/build/grc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tom/gr-hpsdr/grc
+# Build directory: /home/tom/gr-hpsdr/build/grc
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
