014d5e076b3dad0f6d838fcf423b95a833d17843
kv0s <kv0s@fab57ed4-c20f-0410-8556-bd0ba87add53>
2013-01-06 00:40:12 +0000

    Initial commit

---
+++ trunk/KV0S/HPSDRBootloader/statusdialog.cpp
@@ -0,0 +1,54 @@
+/*
+ * File:  statusdialog.cpp
+ *
+ * Revised on December 30, 2012
+ * Author: Dave Larsen, KV0S
+ */
+
+/* Copyright (C)
+* 2009 - John Melton, G0ORX/N6LYT
+* 2012 - Dave Larsen, KV0S
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
+
+#include "statusdialog.h"
+#include "ui_statusdialog.h"
+
+StatusDialog::StatusDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::StatusDialog)
+{
+    ui->setupUi(this);
+
+    connect(ui->closeButton,SIGNAL(clicked()),this,SLOT(close()));
+}
+
+StatusDialog::~StatusDialog()
+{
+    delete ui;
+}
+
+void StatusDialog::status( QString text )
+{
+    qDebug()<<"status:"<<text;
+    ui->statusListWidget->insertItem(ui->statusListWidget->count()-1,text);
+    ui->statusListWidget->setCurrentRow(ui->statusListWidget->count()-1);
+}
+
+void StatusDialog::clear()
+{
+    ui->statusListWidget->clear();
+}
