37c5ccf97b81552c05257891911038b995acc6dc
Tom McDermott, N5EG <Tom-McDermott@users.noreply.github.com>
2013-07-08 21:56:25 +0000

    Create GRC3.6 branch.

---
+++ N5EG/GRC3.6/gr-hpsdr/build/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tom/gr-hpsdr/include
+# Build directory: /home/tom/gr-hpsdr/build/include
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
