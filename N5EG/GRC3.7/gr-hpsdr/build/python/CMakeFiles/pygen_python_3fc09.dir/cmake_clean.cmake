ad664a04030480abdfc416ec07ba4157b7c7e526
Tom McDermott, N5EG <Tom-McDermott@users.noreply.github.com>
2013-07-08 21:49:09 +0000

    GRC3.7 changes namespaces and module function names

---
+++ N5EG/GRC3.7/gr-hpsdr/build/python/CMakeFiles/pygen_python_3fc09.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pygen_python_3fc09"
+  "__init__.pyc"
+  "__init__.pyo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/pygen_python_3fc09.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
