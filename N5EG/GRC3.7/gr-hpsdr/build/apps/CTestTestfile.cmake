2ecc7956674219bb6f01f61e0d7d7431dad4b29a
Tom McDermott, N5EG <Tom-McDermott@users.noreply.github.com>
2013-07-10 22:17:12 +0000

    Remove Ver3.6 files from 3.7 branch

--- N5EG/GRC3.7/gr-hpsdr/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/gr-hpsdr/apps
-# Build directory: /home/tom/gr-hpsdr/build/apps
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
