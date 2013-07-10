8d95d88da460f235f07d6745b97f39ac14608bed
n5eg <n5eg@fab57ed4-c20f-0410-8556-bd0ba87add53>
2013-07-10 22:17:12 +0000

    Remove Ver3.6 files from 3.7 branch

--- trunk/N5EG/GRC3.7/gr-hpsdr/build/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/gr-hpsdr/docs
-# Build directory: /home/tom/gr-hpsdr/build/docs
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
