2f5b9564d7fafb5fbb662bceed6d1c1efc444b8a
Tom McDermott, N5EG <Tom-McDermott@users.noreply.github.com>
2013-07-21 18:48:35 +0000

    Reorganize folders

--- N5EG/GRC3.7/build/include/hpsdr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/gr-hpsdr/include/hpsdr
-# Build directory: /home/tom/gr-hpsdr/build/include/hpsdr
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
