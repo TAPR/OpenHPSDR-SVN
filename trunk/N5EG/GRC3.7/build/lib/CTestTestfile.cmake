2e054c0bc46deb316baba4dbfd48e17dfdbdf75b
n5eg <n5eg@fab57ed4-c20f-0410-8556-bd0ba87add53>
2013-07-21 18:48:35 +0000

    Reorganize folders

--- trunk/N5EG/GRC3.7/build/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/gr-hpsdr/lib
-# Build directory: /home/tom/gr-hpsdr/build/lib
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_hpsdr "/bin/sh" "/home/tom/gr-hpsdr/build/lib/test_hpsdr_test.sh")
