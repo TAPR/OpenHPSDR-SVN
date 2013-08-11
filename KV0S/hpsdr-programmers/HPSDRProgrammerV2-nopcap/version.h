4cce8421b50697e0184dc532e83469b92fe31c22
Dave Larsen <kv0s.dave@gmail.com>
2013-08-11 15:54:49 +0000

    adding Nopcap code to the HPSDRProgrammer suite

--- KV0S/hpsdr-programmers/HPSDRProgrammerV2-nopcap/version.h
+++ KV0S/hpsdr-programmers/HPSDRProgrammerV2-nopcap/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "(version 2.0.3.3)"
-#define RELEASE "2013-7-13"
+#define VERSION "(version 2.0.3.4)"
+#define RELEASE "2013-8-10"
 
 // KV0S revise interface
 // Version 2.0 move to Qt5,  revise GUI
