2dc27a6fc481f6a1e06763677b7af52f65e64299
kv0s <kv0s@fab57ed4-c20f-0410-8556-bd0ba87add53>
2013-12-01 17:21:32 +0000

    Update version numbers

--- trunk/KV0S/hpsdr-programmers/HPSDRProgrammerV2-nopcap/version.h
+++ trunk/KV0S/hpsdr-programmers/HPSDRProgrammerV2-nopcap/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "(version 2.0.4.4)"
-#define RELEASE "2013-11-25"
+#define VERSION "(version 2.0.4.5)"
+#define RELEASE "2013-12-1"
 
 // KV0S revise interface
 // Version 2.0 move to Qt5,  revise GUI
