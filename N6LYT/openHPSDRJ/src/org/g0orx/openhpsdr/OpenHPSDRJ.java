17955ec571c560810e90a659a7e308c82a6fc179
John Melton <john.d.melton@googlemail.com>
2015-04-12 14:46:24 +0000

    Changed UI manager to cross platform so looks same on all platforms.

--- N6LYT/openHPSDRJ/src/org/g0orx/openhpsdr/OpenHPSDRJ.java
+++ N6LYT/openHPSDRJ/src/org/g0orx/openhpsdr/OpenHPSDRJ.java
@@ -5,8 +5,13 @@
  */
 package org.g0orx.openhpsdr;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.UIManager;
+
 import org.g0orx.openhpsdr.wdsp.WDSP;
 
 /**
@@ -19,16 +24,16 @@ public class OpenHPSDRJ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Log.i("OpenHPSDRJ", "Running on: "+System.getProperty("os.name"));
-        Log.i("OpenHPSDRJ", "Current working directory: "+System.getProperty("user.dir"));
+        Log.i("OpenHPSDRJ", "Running on: " + System.getProperty("os.name"));
+        Log.i("OpenHPSDRJ", "Current working directory: " + System.getProperty("user.dir"));
         //System.setProperty("java.library.path", System.getProperty("user.dir")+File.separator+"lib");
         //Log.i("OpenHPSDRJ", "java.library.path: "+System.getProperty("java.library.path"));
         try {
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         } catch (Exception e) {
-            Log.i("OpenHPSDRJ",e.toString());
+            Log.i("OpenHPSDRJ", e.toString());
         }
-        Radio radio=new Radio();
+        Radio radio = new Radio();
     }
-    
+
 }
